bdcf0c494c6d85ef10118174e3b29354aa01934c
Xi Sun <xi@cruncho.com>
2024-03-12 14:13:27 +0100

    log page

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+# from app.views import text_to_audio, grapheme_log_view
 
 import views
 
@@ -7,4 +8,10 @@ interface = Blueprint('interface', __name__)
 interface.add_url_rule(
     '/', view_func=views.text_to_audio,
     methods=['GET', 'POST']
+)
+
+interface.add_url_rule(
+    '/grapheme-log/<int:grapheme_id>',
+    view_func=views.grapheme_log_view,
+    methods=['GET']
 )
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, url_for, redirect
-
+import datetime
 import os
 
 import tts_reader
@@ -20,4 +20,28 @@ def text_to_audio():
 
     return render_template('text-to-audio.html')
 
-            
+
+def grapheme_log_view(grapheme_id):
+    # from app.db_utils import fetch_grapheme_logs, fetch_grapheme
+    # from app.models import Grapheme
+
+    grapheme_logs = [
+    ('ruislip', 'ɹˈaɪslɪp', 'ɹˈaɪslɪp', datetime.datetime(2024, 3, 11, 14, 53, 55, 20387)),
+    ('ruislip', 'ɹˈaɪslɪpi:p', 'ɹˈaɪslɪp', datetime.datetime(2024, 3, 11, 14, 40, 47, 976755)),
+    ('ruislip', 'sɒbaka', 'ɹˈaɪslɪpi:p', datetime.datetime(2024, 3, 11, 14, 40, 32, 181943)),
+    ('ruislip', 'ɹˈaɪslɪp', 'sɒbaka', datetime.datetime(2024, 3, 11, 14, 39, 50, 972417)),
+    ('ruislip', 'ɹˈuːɪslɪp', 'ɹˈaɪslɪp', datetime.datetime(2024, 3, 11, 14, 39, 3, 72773)),
+    ('ruislip', 'ɹˈaɪslɪp', 'ɹˈuːɪslɪp', datetime.datetime(2024, 3, 11, 14, 38, 26, 49876)),
+    ('ruislip', 'ɹˈuːɪslˈɪp', 'ɹˈaɪslɪp', datetime.datetime(2024, 3, 11, 14, 38, 14, 283224)),
+    ('ruislip', 'ɹˈuːɪslɪp', 'ɹˈuːɪslˈɪp', datetime.datetime(2024, 3, 11, 14, 37, 18, 210686)),
+    ('ruislip', "ɹ'aɪslɪp", 'ɹˈuːɪslɪp', datetime.datetime(2024, 3, 11, 14, 35, 57, 696221)),
+    ('ruislip', None, "ɹ'aɪslɪp", datetime.datetime(2024, 3, 11, 14, 20, 27, 147224))]
+    grapheme, phoneme = ['ruislip', 'ruislip']
+
+    return render_template(
+        'grapheme-log.html', 
+        grapheme=grapheme,
+        phoneme=phoneme,
+        grapheme_logs=grapheme_logs)
+
+
